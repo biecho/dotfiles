53a63b18f13b6253ec6fd208c85b33310dd8559b
biecho <diego.meyer@u.nus.edu>
2025-12-01 13:25:50 +0000

    Add Clipboard integration

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -14,3 +14,17 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 if vim.fn.has("nvim-0.9") == 1 then
   vim.g.kitty_protocol = "all"
 end
+
+-- OSC 52 clipboard (allows yanking to local clipboard over SSH)
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
