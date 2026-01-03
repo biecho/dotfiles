41eac7a6d703357aead888a08859b3f80c52b6b0
dmeyer <diego.a.meyer@gmail.com>
2026-01-03 10:01:17 +0000

    Fix clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- OSC 52 clipboard support for SSH sessions (copy to local Mac clipboard)
+if vim.env.SSH_CONNECTION then
+  local osc52 = require("vim.ui.clipboard.osc52")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+  -- Make regular yank/paste use the clipboard (so 'y' copies to Mac clipboard)
+  vim.opt.clipboard = "unnamedplus"
+end
