617a5a8e6fb7fe4a50f0456090e5a5ab29265a8e
dmeyer <diego.a.meyer@gmail.com>
2026-01-03 13:25:30 +0000

    Markdown autocompletion off

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,12 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      trigger = {
+        -- Disable automatic completion popup
+        show_on_insert_on_trigger_character = false,
+        show_on_x_blocked_trigger_characters = {},
+      },
+    },
+  },
+}
