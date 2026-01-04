150de42a52d37daac5d34b11b55073807ddac5de
dmeyer <diego.a.meyer@gmail.com>
2026-01-04 03:58:58 +0000

    Fix markdown preview browser opening over SSH
    The previous implementation used an invalid kitty command (open-url)
    which prevented the browser from opening when using markdown preview
    over SSH. Changed to use the correct 'kitty @ action open_url' command.
    
    Also added:
    - Port forwarding configuration in kitty/ssh.conf for port 9765
    - Installation of markdown-preview.nvim dependencies in setup scripts
    - Browser opening via kitty remote control for SSH sessions

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -20,6 +20,20 @@ return {
     vim.g.mkdp_auto_close = 1
     -- Fixed port for SSH forwarding
     vim.g.mkdp_port = '9765'
+
+    -- Open browser on local machine when using SSH via kitty remote control
+    if vim.env.SSH_CONNECTION or vim.env.SSH_TTY then
+      vim.g.mkdp_browserfunc = 'OpenMarkdownPreviewBrowser'
+      vim.cmd([[
+        function! OpenMarkdownPreviewBrowser(url)
+          " Replace remote address with localhost since we're using port forwarding
+          let l:local_url = substitute(a:url, 'http://127\.0\.0\.1:', 'http://localhost:', '')
+          " Use kitty remote control to open URL on local machine
+          call system('kitty @ action open_url ' . l:local_url)
+        endfunction
+      ]])
+    end
+
     -- Auto scroll sync between editor and preview
     vim.g.mkdp_preview_options = {
       mkit = {},
