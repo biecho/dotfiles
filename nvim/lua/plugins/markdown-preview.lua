1192c9fa5afebbfd0f6085b1cd269350445455ae
dmeyer <diego.a.meyer@gmail.com>
2026-01-06 07:24:45 +0000

    Update markdown fix

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -5,9 +5,7 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
+  build = "cd app && npm install",
   keys = {
     {
       "<leader>mp",
