3b9335cb5405a7bd6e7c687a1300385ed5309097
dmeyer <diego.a.meyer@gmail.com>
2026-01-07 01:23:52 +0000

    Use dynamic port range for markdown preview over SSH
    - Change from fixed port 9765 to range 9700-9799 based on PID
    - Allows multiple SSH sessions with concurrent previews
    - Add notification showing which port is being used

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -19,9 +19,18 @@ return {
 
     -- Open browser on local machine when using SSH via kitty remote control
     if vim.env.SSH_CONNECTION or vim.env.SSH_TTY then
-      -- Fixed port for SSH forwarding
-      vim.g.mkdp_port = '9765'
+      -- Use port range 9700-9799 based on PID for multiple SSH sessions
+      -- Each neovim instance gets a unique port within the forwarded range
+      local port = 9700 + (vim.fn.getpid() % 100)
+      vim.g.mkdp_port = tostring(port)
       vim.g.mkdp_browserfunc = 'OpenMarkdownPreviewBrowser'
+      -- Notify user which port is being used (helpful for SSH forwarding)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MarkdownPreviewToggle",
+        callback = function()
+          vim.notify("Markdown Preview using port " .. port .. " (ensure SSH forwarding)", vim.log.levels.INFO)
+        end,
+      })
       vim.cmd([[
         function! OpenMarkdownPreviewBrowser(url)
           " Replace remote address with localhost since we're using port forwarding
