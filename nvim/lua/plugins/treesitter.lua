f27f9a2ced85cbeda354988da0850126608b4320
dmeyer <diego.a.meyer@gmail.com>
2026-01-09 09:48:24 +0800

    Add assignment navigation and textobjects (]=, [=, a=, i=)
    Uses @assignment.outer from treesitter-textobjects to jump between
    assignments/class fields and select them as textobjects.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 -- Treesitter configuration
--- Note: Requires tree-sitter-cli 0.22.x (newer versions need GLIBC 2.39)
+-- Note: Requires tree-sitter-cli 0.23.x (newer versions need GLIBC 2.39)
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -19,6 +19,33 @@ return {
       auto_install = false, -- Don't auto-install, use ensure_installed list
     },
   },
+
+  -- Add ]=/[= to jump between assignments (class fields, variables, etc.)
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    opts = function(_, opts)
+      opts.move = opts.move or {}
+      opts.move.keys = opts.move.keys or {}
+      opts.move.keys.goto_next_start = opts.move.keys.goto_next_start or {}
+      opts.move.keys.goto_next_start["]="] = "@assignment.outer"
+      opts.move.keys.goto_previous_start = opts.move.keys.goto_previous_start or {}
+      opts.move.keys.goto_previous_start["[="] = "@assignment.outer"
+    end,
+  },
+
+  -- Add a=/i= textobjects for assignments
+  {
+    "nvim-mini/mini.ai",
+    opts = function(_, opts)
+      local ai = require("mini.ai")
+      opts.custom_textobjects = opts.custom_textobjects or {}
+      opts.custom_textobjects["="] = ai.gen_spec.treesitter({
+        a = "@assignment.outer",
+        i = "@assignment.inner",
+      })
+    end,
+  },
+
   -- Prevent Mason from installing tree-sitter-cli (we use npm version)
   {
     "mason-org/mason.nvim",
