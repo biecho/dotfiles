df1f3d469ba44984dd05a709079729bec3a26887
biecho <diego.meyer@u.nus.edu>
2026-01-15 11:36:02 +0800

    Use `-` for Yazi (vim-vinegar style)
    - `-` opens Yazi at current file (no leader, faster)
    - `<leader>-` opens Yazi at cwd (let LazyVim have its split)
    - `<C-Up>` resumes Yazi

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -5,18 +5,11 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   event = "VeryLazy",
   keys = {
-    { "<leader>cw", "<cmd>Yazi cwd<cr>", desc = "Yazi (cwd)" },
+    -- `-` is the classic vim-vinegar pattern for "go to directory"
+    { "-", "<cmd>Yazi<cr>", desc = "Yazi (current file)" },
+    { "<leader>-", "<cmd>Yazi cwd<cr>", desc = "Yazi (cwd)" },
     { "<c-up>", "<cmd>Yazi toggle<cr>", desc = "Yazi (resume)" },
   },
-  -- Use init to set <leader>- AFTER LazyVim keymaps load
-  init = function()
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "VeryLazy",
-      callback = function()
-        vim.keymap.set("n", "<leader>-", "<cmd>Yazi<cr>", { desc = "Yazi (current file)" })
-      end,
-    })
-  end,
   opts = {
     -- Don't replace netrw for directory arguments
     open_for_directories = false,
