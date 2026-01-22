ed37c82f0cd27a668fbeb04f7e7fcedb74388477
biecho <diego.meyer@u.nus.edu>
2026-01-22 16:21:27 +0800

    Enable treesitter directly loading for markdown

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -18,4 +18,20 @@ return {
       },
     },
   },
+  -- Ensure treesitter highlighting attaches for markdown (fixes SSH timing issues)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(args)
+          vim.schedule(function()
+            if vim.api.nvim_buf_is_valid(args.buf) then
+              vim.treesitter.start(args.buf, "markdown")
+            end
+          end)
+        end,
+      })
+    end,
+  },
 }
