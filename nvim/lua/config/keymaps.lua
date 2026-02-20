10a540b8a418a43c4bb1fd74d2d2b5bb9f1cad49
biecho <diego.meyer@u.nus.edu>
2026-02-20 10:23:48 +0800

    Clean up keybinding conflicts between nvim and yazi
    - Remove yazi gd/gr/gp shortcuts that overlap with nvim Lspsaga
    - Remove nvim <leader>yf/yF/yn path-copy shortcuts (handled by yazi cp/cn/cd)
    - Move diagnostic yank from <leader>yd/yD to <leader>xy/xY (LazyVim convention)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,22 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Copy filename to clipboard
-vim.keymap.set("n", "<leader>yf", function()
-  vim.fn.setreg("+", vim.fn.expand("%"))
-  vim.notify("Copied relative path: " .. vim.fn.expand("%"))
-end, { desc = "Yank filename (relative path)" })
-
-vim.keymap.set("n", "<leader>yF", function()
-  vim.fn.setreg("+", vim.fn.expand("%:p"))
-  vim.notify("Copied absolute path: " .. vim.fn.expand("%:p"))
-end, { desc = "Yank filename (absolute path)" })
-
-vim.keymap.set("n", "<leader>yn", function()
-  vim.fn.setreg("+", vim.fn.expand("%:t"))
-  vim.notify("Copied filename: " .. vim.fn.expand("%:t"))
-end, { desc = "Yank filename only" })
-
 -- Helper to format and yank diagnostics
 local function yank_diagnostics(diagnostics, scope_desc)
   if #diagnostics == 0 then
@@ -37,19 +21,19 @@ local function yank_diagnostics(diagnostics, scope_desc)
 end
 
 -- Yank diagnostics on current line
-vim.keymap.set("n", "<leader>yd", function()
+vim.keymap.set("n", "<leader>xy", function()
   local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
   yank_diagnostics(diagnostics, "on current line")
 end, { desc = "Yank diagnostics on line" })
 
 -- Yank all diagnostics in file
-vim.keymap.set("n", "<leader>yD", function()
+vim.keymap.set("n", "<leader>xY", function()
   local diagnostics = vim.diagnostic.get(0)
   yank_diagnostics(diagnostics, "in file")
 end, { desc = "Yank all diagnostics in file" })
 
 -- Yank diagnostics in visual selection
-vim.keymap.set("v", "<leader>yd", function()
+vim.keymap.set("v", "<leader>xy", function()
   -- Exit visual mode to populate '< and '> marks
   vim.cmd([[execute "normal! \<Esc>"]])
   local start_line = vim.fn.line("'<") - 1 -- 0-indexed
