e99e0891e1877555219a5269443275ae33cdb440
biecho <diego.meyer@u.nus.edu>
2026-02-24 19:50:04 +0800

    Export

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -14,6 +14,12 @@ return {
 
       -- View staged changes (non-conflicting with LazyVim's <leader>gs)
       { "<leader>gc", "<cmd>DiffviewOpen --staged<cr>", desc = "Diff Cached/Staged" },
+
+      -- Review branch changes against main
+      { "<leader>gm", "<cmd>DiffviewOpen origin/main...HEAD<cr>", desc = "Diff vs Main" },
+
+      -- View last commit
+      { "<leader>gh", "<cmd>DiffviewOpen HEAD~1<cr>", desc = "Diff Last Commit (HEAD)" },
     },
     opts = {
       enhanced_diff_hl = true, -- Better syntax highlighting in diffs
@@ -29,6 +35,45 @@ return {
         win_config = {
           width = 35,
         },
+      },
+    },
+  },
+
+  -- Octo: GitHub PR/issue review inside Neovim
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>go", "<cmd>Octo<cr>", desc = "Octo (GitHub)" },
+      { "<leader>gpl", "<cmd>Octo pr list<cr>", desc = "List PRs" },
+      { "<leader>gps", "<cmd>Octo pr search<cr>", desc = "Search PRs" },
+      { "<leader>gil", "<cmd>Octo issue list<cr>", desc = "List Issues" },
+      { "<leader>gis", "<cmd>Octo issue search<cr>", desc = "Search Issues" },
+    },
+    opts = {
+      enable_builtin = true, -- Use telescope picker for Octo commands
+      default_merge_method = "squash",
+      ssh_aliases = {},
+    },
+  },
+
+  -- git-conflict: Inline merge conflict resolution
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    event = "BufReadPre",
+    opts = {
+      default_mappings = true, -- co=ours, ct=theirs, cb=both, c0=none
+      default_commands = true, -- GitConflictChooseOurs, etc.
+      disable_diagnostics = true, -- Less noise during conflict resolution
+      highlights = {
+        incoming = "DiffAdd",
+        current = "DiffText",
       },
     },
   },
