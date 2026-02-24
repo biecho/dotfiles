a42036eaebce4c26c6f58aaf7ab75858d9560dc7
biecho <diego.meyer@u.nus.edu>
2026-02-24 19:50:04 +0800

    Export

--- nvim/lua/plugins/smart-splits.lua
+++ nvim/lua/plugins/smart-splits.lua
@@ -17,11 +17,11 @@ return {
     { "<C-j>", function() require("smart-splits").move_cursor_down() end, desc = "Move to split below" },
     { "<C-k>", function() require("smart-splits").move_cursor_up() end, desc = "Move to split above" },
     { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
-    -- Resizing (alt+hjkl)
-    { "<A-h>", function() require("smart-splits").resize_left() end, desc = "Resize split left" },
-    { "<A-j>", function() require("smart-splits").resize_down() end, desc = "Resize split down" },
-    { "<A-k>", function() require("smart-splits").resize_up() end, desc = "Resize split up" },
-    { "<A-l>", function() require("smart-splits").resize_right() end, desc = "Resize split right" },
+    -- Resizing (ctrl+alt+hjkl, alt+hjkl is reserved for AeroSpace)
+    { "<C-A-h>", function() require("smart-splits").resize_left() end, desc = "Resize split left" },
+    { "<C-A-j>", function() require("smart-splits").resize_down() end, desc = "Resize split down" },
+    { "<C-A-k>", function() require("smart-splits").resize_up() end, desc = "Resize split up" },
+    { "<C-A-l>", function() require("smart-splits").resize_right() end, desc = "Resize split right" },
     -- Swap buffers (leader+hjkl)
     { "<leader><leader>h", function() require("smart-splits").swap_buf_left() end, desc = "Swap buffer left" },
     { "<leader><leader>j", function() require("smart-splits").swap_buf_down() end, desc = "Swap buffer down" },
