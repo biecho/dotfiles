1f10628e763c66dcc59c24d91720d9eeb91eae9d
biecho <diego.meyer@u.nus.edu>
2026-02-28 15:36:46 +0800

    Remove Jupyter/notebook support from nvim config

--- nvim/after/queries/markdown/textobjects.scm
+++
@@ -1,3 +0,0 @@
-; Code cell text objects for notebook navigation
-(fenced_code_block
-  (code_fence_content) @code_cell.inner) @code_cell.outer
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Python provider for Molten/nvim plugins (dedicated venv created by install script)
-vim.g.python3_host_prog = vim.fn.expand("~/.local/share/nvim/python/bin/python")
-
 -- OSC 52 clipboard support for SSH sessions (copy to local Mac clipboard)
 if vim.env.SSH_CONNECTION then
   local osc52 = require("vim.ui.clipboard.osc52")
