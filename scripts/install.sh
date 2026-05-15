33569d96ac0a50b098584b101d536c6925385d98
biecho <diego.meyer@u.nus.edu>
2026-05-15 17:52:35 +0800

    Default Ansible provisioning to local target

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,18 +3,4 @@ set -euo pipefail
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-has_limit=false
-for arg in "$@"; do
-    case "$arg" in
-        -l|--limit|--limit=*)
-            has_limit=true
-            break
-            ;;
-    esac
-done
-
-if "$has_limit"; then
-    exec ansible-playbook "$DOTFILES_DIR/ansible/playbook.yml" "$@"
-else
-    exec ansible-playbook "$DOTFILES_DIR/ansible/playbook.yml" --limit local "$@"
-fi
+exec ansible-playbook "$DOTFILES_DIR/ansible/playbook.yml" "$@"
